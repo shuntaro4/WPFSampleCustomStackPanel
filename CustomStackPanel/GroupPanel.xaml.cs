16901176bf48c74dc6eb63008b8e2972a266f26a
shuntaro4 <shuntaro4@gmail.com>
2018-09-29 16:10:21 +0900

    add: UserControl - GroupPanel を追加

---
+++ CustomStackPanel/GroupPanel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CustomStackPanel
+{
+    /// <summary>
+    /// GroupPanel.xaml の相互作用ロジック
+    /// </summary>
+    public partial class GroupPanel : UserControl
+    {
+        public static DependencyProperty StyleColorProperty =
+            DependencyProperty.Register(
+                "StyleColor",
+                typeof(Brush),
+                typeof(GroupPanel),
+                new PropertyMetadata(new SolidColorBrush(Colors.Transparent)));
+
+        public Brush StyleColor
+        {
+            get => (Brush)GetValue(StyleColorProperty);
+            set { SetValue(StyleColorProperty, value); }
+        }
+
+        public static DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                "Title",
+                typeof(string),
+                typeof(GroupPanel),
+                new PropertyMetadata(""));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public GroupPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
